85ce041d774bf7832e87743bf534e8a796090c04
Jaime Johnsen <drivetraindiva@gmail.com>
2020-10-29 17:07:47 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 
 Hashie
-Sinatra //classes to be defined by gems if installed properly
+Sinatra #classes to be defined by gems if installed properly
 Octokit
 Pry
 AwesomePrint
